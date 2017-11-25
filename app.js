88e5bcfa699685ba9ac0e1966de57e6fc329f286
Rabin Gaire <rabingaire20@gmail.com>
2017-11-25 12:36:37 +0545

    added form

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const port = 3000;
 
 const app = express();
 
+const path = require('path');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
   res.end("Hello World!");
+});
+
+app.get('/hello_form', function(req, res) {
+  res.sendFile(path.join(__dirname + '/views/hello_form.html'));
+});
+
+app.post('/hello', function (req, res) {
+  res.end(`Hello ${req.body.name}!`);
 });
 
 app.listen(port, () => {
